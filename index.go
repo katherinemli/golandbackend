a4b42f1578e93eee76a6421f4d6f75898ede8a35
katherinemli <katherine.lib.ira@gmail.com>
2024-01-29 15:46:43 -0500

    get and post

--- index.go
+++ index.go
@@ -1,20 +1,36 @@
 package main
 
 import (
-	"io"
 	"log"
+	"fmt"
 	"net/http"
 	"os"
+	"github.com/rs/cors"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	port := os.Getenv("PORT")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", getOneEvent).Methods("GET")
+	router.HandleFunc("/createRoute/{id}", createRouter).Methods("POST")
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:8080", "https://ornate-shortbread-20015a.netlify.app"},
+		AllowCredentials: true,
+	})
+	fmt.Println("entre")
+	//os.Setenv("PORT", "3000")
+	handler := c.Handler(router)
+	port := os.Getenv(("PORT"))
+	log.Fatal(http.ListenAndServe(":"+port, handler))
+}
+func createRouter(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("entre")
+}
+func getOneEvent(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("entre")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
-	}
-
-	http.HandleFunc("/", helloHandler)
-	log.Println("Listing for" + port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	/* json.NewEncoder(w).Encode(allAddress) */
 }
