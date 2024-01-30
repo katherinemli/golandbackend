afee879ad53133a4d592a4e498cf29bdba010619
katherinemli <katherine.lib.ira@gmail.com>
2024-01-30 14:15:53 -0500

    array object

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "net/http"
+)
+
+func check(e error) {
+    if e != nil {
+        fmt.Println(e)
+        panic(e)
+    }
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Index functoin")
+    indexHTML, err := ioutil.ReadFile("datachart.txt")
+    check(err)
+    fmt.Println(indexHTML)
+    w.Write(indexHTML)
+}
+
+func main() {
+    http.HandleFunc("/", Index)
+
+    err := http.ListenAndServe(":"+os.Getenv("PORT"),, nil)
+    check(err)
+}
