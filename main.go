9b4747cf185419ded7ca64afae5672bc588eccba
katherinemli <katherine.lib.ira@gmail.com>
2024-01-30 15:28:11 -0500

    array object

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
     "fmt"
     "io/ioutil"
+	"encoding/json"
     "net/http"
+	"strconv"
+    "strings"
 	"os"
 )
-
+type chartData struct {
+    Values []int `json:"value"`
+}
 func check(e error) {
     if e != nil {
         fmt.Println(e)
@@ -15,16 +20,38 @@ func check(e error) {
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("Index functoin")
-    indexHTML, err := ioutil.ReadFile("datachart.txt")
-    check(err)
-    fmt.Println(indexHTML)
-    w.Write(indexHTML)
+    nums, err := readFile("datachart.txt")
+    if err != nil { panic(err) }
+    fmt.Println(nums)
+    resProcess := &chartData{Values: nums}
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(resProcess)
 }
+func readFile(fname string) (nums []int, err error) {
+    b, err := ioutil.ReadFile(fname)
+    if err != nil { return nil, err }
 
+    lines := strings.Split(string(b), "\n")
+    // Assign cap to avoid resize on every append.
+    nums = make([]int, 0, len(lines))
+
+    for _, l := range lines {
+        // Empty line occurs at the end of the file when we use Split.
+        if len(l) == 0 { continue }
+        // Atoi better suits the job when we know exactly what we're dealing
+        // with. Scanf is the more general option.
+        n, err := strconv.Atoi(l)
+        if err != nil { return nil, err }
+        nums = append(nums, n)
+    }
+
+    return nums, nil
+}
 func main() {
     http.HandleFunc("/", Index)
-
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+    //err := http.ListenAndServe(":8080", nil)
     check(err)
+	
 }
