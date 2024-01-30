02cc26dd96f05c65870d10e4f43a93a97e2cf37a
katherinemli <katherine.lib.ira@gmail.com>
2024-01-30 10:42:40 -0500

    get and post

--- index.go
+++ index.go
@@ -22,7 +22,7 @@ func main() {
 	router.HandleFunc("/", getOneEvent).Methods("GET")
 	router.HandleFunc("/createRoute/{id}", createRouter).Methods("POST")
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://10.11.14.50", "https://ornate-shortbread-20015a.netlify.app"},
+		AllowedOrigins:   []string{"10.11.14.50", "https://ornate-shortbread-20015a.netlify.app"},
 		AllowCredentials: true,
 	})
 	//os.Setenv("PORT", "3000")
@@ -33,6 +33,67 @@ func main() {
 }
 func createRouter(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("entre")
+	fmt.Println("entre")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	bolB, _ := json.Marshal(true)
+    fmt.Println(string(bolB))
+
+    intB, _ := json.Marshal(1)
+    fmt.Println(string(intB))
+
+    fltB, _ := json.Marshal(2.34)
+    fmt.Println(string(fltB))
+
+    strB, _ := json.Marshal("gopher")
+    fmt.Println(string(strB))
+
+    slcD := []string{"apple", "peach", "pear"}
+    slcB, _ := json.Marshal(slcD)
+    fmt.Println(string(slcB))
+
+    mapD := map[string]int{"apple": 5, "lettuce": 7}
+    mapB, _ := json.Marshal(mapD)
+    fmt.Println(string(mapB))
+
+    res1D := &response1{
+        Page:   1,
+        Fruits: []string{"apple", "peach", "pear"}}
+    res1B, _ := json.Marshal(res1D)
+    fmt.Println(string(res1B))
+
+    res2D := &response2{
+        Page:   1,
+        Fruits: []string{"apple", "peach", "pear"}}
+    res2B, _ := json.Marshal(res2D)
+    fmt.Println(string(res2B))
+
+    byt := []byte(`{"num":6.13,"strs":["a","b"]}`)
+
+    var dat map[string]interface{}
+
+    if err := json.Unmarshal(byt, &dat); err != nil {
+        panic(err)
+    }
+    fmt.Println(dat)
+
+    num := dat["num"].(float64)
+    fmt.Println(num)
+
+    strs := dat["strs"].([]interface{})
+    str1 := strs[0].(string)
+    fmt.Println(str1)
+
+    str := `{"page": 1, "fruits": ["apple", "peach"]}`
+    res := response2{}
+    json.Unmarshal([]byte(str), &res)
+    fmt.Println(res)
+    fmt.Println(res.Fruits[0])
+
+    enc := json.NewEncoder(os.Stdout)
+    d := map[string]int{"apple": 5, "lettuce": 7}
+	enc.Encode(d)
 }
 func getOneEvent(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("entre")
